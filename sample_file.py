94267ef62c067ac0e96a684e21f609cf0485bc8e
Parsankamusa <parsankamusa@gmail.com>
2022-11-26 00:45:25 +0300

    sample file

---
+++ sample_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self,  width, length):
+        self.width = 10
+        self.length = 12
+        return self.idth * self.length
+
+        
+        R = Rectangle(width, length)
+        
+
+        print("the area is:", R.area)
+        print()       
